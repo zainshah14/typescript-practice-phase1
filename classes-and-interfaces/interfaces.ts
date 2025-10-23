faa2f3c75170b2ed702115d438dd42c4e1195789
Zain Shah <zainshah@Zains-MacBook-Air.local>
2025-10-23 23:05:59 +0500

    Section 6 – lectures 84–87: interface implementation & wrap-up

--- classes-and-interfaces/interfaces.ts
+++ classes-and-interfaces/interfaces.ts
@@ -6,15 +6,39 @@ interface Authenticatable {
     logout(): void;
 }
 
+// interface Authenticatable {
+//     role: string;
+// }
+
+interface AuthenticatableAdmin extends Authenticatable {
+    role: 'admin' | 'superadmin';
+}
+
+class AuthenticatableUser implements Authenticatable {
+    constructor(public userName: string, public email: string, public password: string) {}
+
+    login() {
+        
+    }
+
+    logout() {
+        
+    }
+}
+
+function authenticate(user: Authenticatable) {
+    user.login();
+}
+
 let user: Authenticatable;
 
 user = {
     email: 'test@example.com',
     password: 'abc1',
     login() {
-        
+        // reach out to a database, check credentials, create a session
     },
     logout() {
-        
+        // clear the session
     },
 }
